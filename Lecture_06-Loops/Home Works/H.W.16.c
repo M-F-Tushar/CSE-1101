0efb0c76be3c6a294c103c8072e6cdd761213360
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-27 19:36:52 +0600

    Update H.W.16.c

--- Lecture_06-Loops/Home Works/H.W.16.c
+++ Lecture_06-Loops/Home Works/H.W.16.c
@@ -9,18 +9,21 @@ int main() {
     int N;
     double sum = 0.0;
 
+    // Input: make sure N is odd and at least 3
     do {
         printf("Enter an odd integer (>=3): ");
         scanf("%d", &N);
-    } while (N < 3 || N % 2 == 0);
+    } while (N < 3 || N % 2 == 0);//While N is less than 3 OR N is even, keep asking for input.
 
+    // Calculate the series
     for (int i = 1; i <= N - 2; i += 2) {
         sum += 1.0 / (i * (i + 2));
     }
 
-
+    // Output the result
     printf("The result of the series is: %.6f\n", sum);
 
     return 0;
 }
+
 
