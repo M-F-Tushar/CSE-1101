b6b9d31c85a8ccf0348d332b8aa5cbd3fdcd3ace
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-27 14:27:59 +0600

    Update 2.c

--- CSE - CT/CT-3/2.c
+++ CSE - CT/CT-3/2.c
@@ -1,44 +1,55 @@
 Q2: Write a program that produces the output shown in Fig. a, Also store the output in a 2D array and print in reverse as in Fig. b
-Fig. a Output:              
-1  2  3  4
-4  3  4  5
-9  4  5  5
-16 5  5  5
-25
+Fig. a:
+1
+2	4
+3	3	9
+4	4	4	16
+5	5	5	5	25
 
-Fig. b Output:
-25
-16 5
-9  4  5
-4  3  4  5
-1  2  3  4
+Fig. b:
+25	5	5	5	5
+16	4	4	4
+9	3	3
+4	2
+1
+
 
 #include <stdio.h>
 
+#define SIZE 5
+
 int main() {
-    int arr[5][4] = {0};
+    int arr[SIZE][SIZE] = {0}; // Initialize array with zeros
+    int i, j;
 
-    // Create and print Fig. a
+    // Step 1: Fill array according to Fig. a
+    for (i = 0; i < SIZE; i++) {
+        for (j = 0; j <= i; j++) {
+            if (i == j)
+                arr[i][j] = (i + 1) * (i + 1); // Square of the number
+            else
+                arr[i][j] = i + 1; // Just the row number
+        }
+    }
+
+    // Print Fig. a
     printf("Fig. a:\n");
-    for (int i = 0; i < 5; i++) {
-        arr[i][0] = (i + 1) * (i + 1);
-        printf("%d ", arr[i][0]);
-
-        for (int j = 1; j < 4 - i; j++) {
-            arr[i][j] = 5 - i + j;
-            printf("%d ", arr[i][j]);
+    for (i = 0; i < SIZE; i++) {
+        for (j = 0; j <= i; j++) {
+            printf("%d\t", arr[i][j]);
         }
         printf("\n");
     }
 
-    // Print reverse as Fig. b
+    // Step 2: Print array in reverse order (Fig. b)
     printf("\nFig. b:\n");
-    for (int i = 4; i >= 0; i--) {
-        for (int j = 0; j < 4 - i; j++) {
-            printf("%d ", arr[i][j]);
+    for (i = SIZE - 1; i >= 0; i--) {
+        for (j = i; j >= 0; j--) {
+            printf("%d\t", arr[i][j]);
         }
         printf("\n");
     }
 
     return 0;
 }
+
