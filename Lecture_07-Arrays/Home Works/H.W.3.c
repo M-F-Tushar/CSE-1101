0eac57e3c7f1d01664f95a4b50b9f9e248f48d55
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-27 20:32:48 +0600

    Update H.W.3.c

--- Lecture_07-Arrays/Home Works/H.W.3.c
+++ Lecture_07-Arrays/Home Works/H.W.3.c
@@ -3,23 +3,24 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main() {
+int main() 
+{
     int arr[10];
     int i, sum = 0;
     float average;
 
-    srand(time(0)); // Seed the random number generator
+    srand(time(NULL)); // Seed the random number generator
 
     // Assign random values and calculate sum
-    for(i = 0; i < 10; i++) {
+    for(i = 0; i < 10; i++) 
+    {
         arr[i] = rand() % 21; // Random number from 0 to 20
         sum += arr[i];
-        printf("%d ", arr[i]); // Print the numbers (optional)
     }
 
     average = sum / 10.0;
 
-    printf("\nAverage = %.2f\n", average);
+    printf("Average = %.2f\n", average);
 
     return 0;
 }
