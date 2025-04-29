80a05a00a6becb2e7acdbc749e43bf39e46178e0
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-29 19:59:53 +0600

    Update H.W.4.c

--- Lecture_07-Arrays/Home Works/H.W.4.c
+++ Lecture_07-Arrays/Home Works/H.W.4.c
@@ -5,21 +5,32 @@ were 1, −9, 5, 3, the rotated array would be 3, 1, −9, 5.
 */
 #include <stdio.h>
 
-int main() 
-{
-    int arr[5] = {1, -9, 5, 3}, temp;
-    int n = 4; // Number of elements
+int main() {
+    int arr[100], i, temp;
 
-    temp = arr[n - 1]; // Store last element
-    for (int i = n - 1; i > 0; i--) 
-    {
-        arr[i] = arr[i - 1];//Shift Elements to the Right
+    // You can change 10 to 100 for full input
+    int n = 10; 
+
+    // Read n integers
+    printf("Enter %d integers:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-    arr[0] = temp; // Move last element to front
 
-    printf("Rotated array: ");
-    for (int i = 0; i < n; i++) 
-    {
+    // Store last element in temp
+    temp = arr[n - 1];
+
+    // Shift elements one position to the right
+    for(i = n - 1; i > 0; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+    // Place last element at first position
+    arr[0] = temp;
+
+    // Print rotated array
+    printf("Rotated array:\n");
+    for(i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
 
