5c8b622c9192ca1da6e650f9e3fc2cf95feed528
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-04-29 19:10:31 +0600

    Update H.W.9.c

--- Lecture_06-Loops/Home Works/H.W.9.c
+++ Lecture_06-Loops/Home Works/H.W.9.c
@@ -7,13 +7,16 @@ int main() {
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    if (num <= 1) {
+    if (num <= 1) 
+    {
         printf("Not a prime number.\n");
         return 0;
     }
 
-    for (i = 2; i < num; i++) {
-        if (num % i == 0) {
+    for (i = 2; i < num; i++) 
+    {
+        if (num % i == 0) 
+        {
             printf("Not a prime number.\n");
             return 0;
         }
